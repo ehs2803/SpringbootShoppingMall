a6754711ab2c090faee607a01796a80ca6b97092
ehs2803 <hr2803@naver.com>
2022-03-15 22:12:39 +0900

    20220315

---
+++ SpringBootShop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,16 @@
+package com.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class ThymeleafExController {
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07(){
+        return "thymeleafEx/thymeleafEx07";
+    }
+}
